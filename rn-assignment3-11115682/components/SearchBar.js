b62e433442b20645c2256771fe5c5838b29efe29
Bouffdaddy1z <derrickedus66@gmail.com>
2024-05-31 13:40:35 +0000

    Added Search Bar to the page

---
+++ rn-assignment3-11115682/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.searchContainer}>
+      <Ionicons name="ios-search" size={20} color="#666" style={styles.searchIcon} />
+      <TextInput placeholder="Search" style={styles.searchInput} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+  },
+});
+
+export default SearchBar;
